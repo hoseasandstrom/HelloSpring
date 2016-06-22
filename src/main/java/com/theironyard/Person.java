6cb586da055da4bdc56c7c93b37c126f35cc032e
hoseasandstrom <Lakyn4ever>
2016-06-22 11:02:45 -0400

    Added JSON routes

--- src/main/java/com/theironyard/Person.java
+++ src/main/java/com/theironyard/Person.java
@@ -13,4 +13,31 @@ public class Person {
         this.city = city;
         this.age = age;
     }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
